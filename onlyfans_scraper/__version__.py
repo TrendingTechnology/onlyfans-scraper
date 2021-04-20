0bb1b2ea4eb1ba9bebc34564c5b325d31e2cf30a
Amenly <60410674+Amenly@users.noreply.github.com>
2021-04-19 22:29:12 -0500

    Update __version__.py

--- onlyfans_scraper/__version__.py
+++ onlyfans_scraper/__version__.py
@@ -8,7 +8,7 @@ r"""
 """
 
 __title__ = 'onlyfans-scraper'
-__version__ = '0.1.0'
+__version__ = '0.3.0'
 __author__ = 'Amenly'
 __author_email__ = 'uamenly@protonmail.com'
 __description__ = 'A command-line program to download media, like posts, and more from creators on OnlyFans.'
